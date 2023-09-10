bf255bc411b00a15c86310cfcbfcb28788441501
Martin Araka <arakamartin@gmail.com>
2023-09-10 16:52:14 -0700

    a program that prints all possible combinations

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - main block/ main entry
+ * Description -  program that prints single-digit numbers.
+ * Numbers must be separated by (,) and a space
+ * You can only use the putchar function
+ * You can only use putchar four times
+ * You are not allowed to use any variable of type char
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		putchar('0' + i);
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}

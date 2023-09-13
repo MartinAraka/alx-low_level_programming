eddf3855a8e91d5c996a1537de43e11018393bfe
Martin Araka <arakamartin@gmail.com>
2023-09-13 11:11:31 -0700

    Print alphabet

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * islower function - function that checks for lowercase character.
+ * @c: parameter to be checked
+ *
+ * Return: 1 0r 0
+ */
+
+int _islower(int c);
+{
+	int c;
+
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+}

50a16781cdec3562abd1728b2729aaaf1fe59983
Martin Araka <arakamartin@gmail.com>
2023-09-15 19:16:38 -0700

    Fizz Buzz test

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - main block
+ * Description A Fizz-Buzz test
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 99; i++)
+	{
+		if ((i % 3 == 0) && (i % 5 == 0))
+			printf("%s", "FizzBuzz");
+		else if (i % 3 == 0)
+			printf("%s", "Fizz");
+		else if (i % 5 == 0)
+			printf("%s", "Buzz");
+		else
+			printf("%d", i);
+		if (i != 100)
+			printf(" ");
+	}
+	printf("\n");
+	return (0);
+}

82437f3b73bc4cf1316f682aa37dd2b69977f5f5
Martin Araka <arakamartin@gmail.com>
2023-09-16 21:02:47 -0700

    Check if lowercase

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -4,7 +4,7 @@
  * _islower - function that checks for lower case character
  * @c: parameter to be checked for
  *
- * Return: 1 if succesful or 1 if failure
+ * Return: 1 if succesful or 0 if failure
  */
 
 int _islower(int c)

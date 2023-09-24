468d3d0242dec5bdbf92762a9a9214215a31ab48
Martin Araka <arakamartin@gmail.com>
2023-09-24 09:27:30 -0700

    Add two integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - function that adds two integers and returns the result
+ * @i: parameter
+ * @j: parameter
+ *
+ * Return: sum
+ */
+
+int add(int i, int j)
+{
+	int sum = (i + j);
+
+	return (sum);
+}
--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -25,14 +25,15 @@ void times_table(void)
 			{
 				_putchar(',');
 				_putchar(' ');
+				_putchar(' ');
 				_putchar(k + '0');
 			}
-			else if (k > 10)
+			else if (k > 10 && j != 0)
 			{
 				_putchar(',');
 				_putchar(' ');
-				_putchar(k / 10 + '0');
-				_putchar(k % 10 + '0');
+				_putchar((k / 10) + '0');
+				_putchar((k % 10) + '0');
 
 			}
 

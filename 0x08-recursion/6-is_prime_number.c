97afa42f1d9d208de1db30f219d6f5202a986df3
Martin Araka <arakamartin@gmail.com>
2023-09-28 09:54:49 -0700

    Study prime number

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,6 +1,28 @@
 #include "main.h"
 
 /**
+ * is_prime_check - returns 1 if integer input is a prime number
+ * @n: parameter for integer
+ * @i: parameter for integer
+ *
+ * Return: 1 if integer is a prime number
+ */
+
+int is_prime_check(int n, int i)
+{
+	if (i > n / 2)
+	{
+		return (1);
+	}
+	if (n % i == 0)
+	{
+		return (0);
+	}
+	return (is_prime_check(n, i + 2));
+
+}
+
+/**
  * is_prime_number -  returns 1 if the input integer is a prime number
  * @n: parameter representing integer
  *
@@ -9,14 +31,15 @@
 
 int is_prime_number(int n)
 {
-	int i;
+	int i = 3;
 
 	if (n <= 1)
-		return (0);
-	for (i = 2; i * i <= n; i++)
 	{
-		if (n % i == 0)
-			return (0);
+		return (0);
 	}
-	return (1);
+	if (n == 2)
+	{
+		return (1);
+	}
+	return (is_prime_check(n, i));
 }

181814f1f84e1194e005462cc66b2b63a91bde65
Martin Araka <arakamartin@gmail.com>
2023-10-04 19:54:34 -0700

    Split strings

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * strtow - function that splits two strings
+ * @str: string
+ * Return: pointer to the resulting string
+ */
+
+char **strtow(char *str)
+{
+	(void)str;
+	return (0);
+}
+

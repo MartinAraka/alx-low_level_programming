afd5d91d34c2c6bcbe7f9e1aaf20d6f1a5db85db
Martin Araka <arakamartin@gmail.com>
2023-10-11 17:44:25 -0700

    Calculator operations

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,60 @@
+/**
+ * op_add - sum of integer
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: sum of integers
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - subtracts integers
+ * @a: integer1
+ * @b: integer2
+ *
+ * Return: subtraction
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - multiplies integers
+ * @a: integer1
+ * @b: integer2
+ *
+ * Return: multiplication
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - divides integers
+ * @a: integer1
+ * @b: integer2
+ *
+ * Return: division
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - returns the remainder
+ * @a: integer1
+ * @b: integer2
+ *
+ * Return: modulus
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}

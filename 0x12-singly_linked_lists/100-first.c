2fad7b48a33c2651ff194ab84ef32a2964b883b9
Martin Araka <arakamartin@gmail.com>
2023-10-20 07:59:58 -0700

    Add before_main function

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+
+/**
+ * print_first - function that prints before main function executed
+ *
+ * Return:-
+ */
+
+void print_first(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}

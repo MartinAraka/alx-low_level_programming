336e22398c7618ab83da2ec67ab21643574fb92b
Martin Araka <arakamartin@gmail.com>
2023-10-23 03:42:36 -0700

    Print all elements

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * print_listint - function that prints all elements of list
+ * @h: pointer to the head of list
+ *
+ * Return: number of nodes in list
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		count++;
+	}
+	return (count);
+}

79aac5444bc5a5bb2b37afb0a02301205531b22f
Martin Araka <arakamartin@gmail.com>
2023-11-02 04:05:41 -0700

    Display information contained in Elf

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,15 +1,28 @@
 #ifndef MAIN_H
 #define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
 #include <sys/types.h>
 #include <sys/stat.h>
-#include <fcntl.h>
-#include <unistd.h>
-#include <stdlib.h>
+#include <string.h>
+#include <stdarg.h>
+#include <errno.h>
+#include <elf.h>
+#include <sys/mman.h>
+
+#define BUFFER_SIZE 1024
+#define _POSIX_C_SOURCE 200809L
+
 
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+void error(int code, const char *format, const char *arg);
+void errorFD(int code,const char *format, int FD);
 
 
 #endif
